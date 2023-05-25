3bac62fca12b299ac6444a9b0d4021d3d399c15a
Socolov Alexandr <82725768+assassin0210@users.noreply.github.com>
2023-05-26 00:29:54 +0300

    add commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,6 @@ import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(
     rootReducer,
     composeEnhancers(applyMiddleware(thunk))
